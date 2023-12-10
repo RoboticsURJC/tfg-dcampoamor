9600a94b3d617f4319527b9ebee7c7b7c09ee46f
dcampoamor <d.campoamor@alumnos.uejc.es>
2023-12-10 17:39:57 +0100

    updating src

--- src/deteccion-objetos-video/deteccion_video.py
+++ src/deteccion-objetos-video/deteccion_video.py
@@ -102,8 +102,8 @@ if __name__ == "__main__":
                     color = [int(c) for c in colors[int(cls_pred)]]
                     print("Se detectó {} en X1: {}, Y1: {}, X2: {}, Y2: {}".format(classes[int(cls_pred)], x1, y1, x2, y2))
                     frame = cv2.rectangle(frame, (x1, y1 + box_h), (x2, y1), color, 5)
-                    cv2.putText(frame, classes[int(cls_pred)], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 5)# Nombre de la clase detectada
-                    cv2.putText(frame, str("%.2f" % float(conf)), (x2, y2 - box_h), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 5) # Certeza de prediccion de la clase
+                    cv2.putText(frame, classes[int(cls_pred)], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)# Nombre de la clase detectada
+                    cv2.putText(frame, str("%.2f" % float(conf)), (x2, y2 - box_h), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 2) # Certeza de prediccion de la clase
         #
         #Convertimos de vuelta a BGR para que cv2 pueda desplegarlo en los colores correctos
         
