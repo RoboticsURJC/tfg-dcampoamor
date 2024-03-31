1525ae6c50e99f0f44608c56465f6aeaa34b78ef
dcampoamor <d.campoamor@alumnos.uejc.es>
2024-03-31 13:45:19 +0200

    updating get3Dpoint

--- src/camera/Camera-Calibration-and-Backprojection/M-CAIT-2013/get3Dpoint.py
+++ src/camera/Camera-Calibration-and-Backprojection/M-CAIT-2013/get3Dpoint.py
@@ -1,7 +1,7 @@
 ###################################################################################
 #                                                                                 #
-#                 Real World Coordinate from Image Coordinate Using               #
-#                 Single Calibrated Camera Based on Analytic Geometry             #
+#        Coordenadas del mundo real a partir de coordenadas de imagen usando      #
+#              Una sola cámara calibrada basada en geometría analítica            #
 #                                                                                 #
 ###################################################################################   
 
@@ -19,43 +19,43 @@ import numpy as np
 import cv2 as cv
 
 # ---------------------------------------------------------------------------------
-# Step 1. Perform camera calibration to obtain extrinsic camera parameters R, T and
-# intrinsic camera parameters fx, fy, cx, cy (done in calibrateCamera.py)
+# Paso 1. Calibrar la cámara para obtener los parámetros extrínsecos R, T y
+# parámetros intrínsecos de la cámara fx, fy, cx, cy (hecho en calibrateCamera.py)
 
-# get the camera matrix from the yaml file
+# obtener la matriz de la cámara desde el archivo yaml
 with open('./LogitechC310.yaml', 'r') as stream:
     try:
         calib_params = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
 
-# extract camera matrix and distortion coefficients from yaaml file
+# extraer la matriz de la cámara y los coeficientes de distorsión del archivo yaml
 camera_matrix = np.array(calib_params['mtx']).reshape(3,3)
 
-# calculates mean of the focal lengths (in pixels)
+# cálculo de la media de las distancias focales (en píxeles)
 fx = camera_matrix[0,0]
 fy = camera_matrix[1,1]
 mean_focul_length = (fx + fy) / 2
 
-# Focul length in mm as specified in the product technical specification
+# Longitud del foco en mm como se especifica en la ficha técnica del producto
 focul_length_in_mm = 4.4
 
-# Calculate pixles per mm
+# Calculo de pixeles por mm
 pixles_per_mm = round(mean_focul_length / focul_length_in_mm)
 # print('Pixles per mm: ', pixles_per_mm)
 
-# Assume pixels per mm is the same for x and y 
+# Asunción de que los píxeles por mm son los mismos para x e y
 sx = sy = pixles_per_mm
 
-# principal point
+# centros ópticos
 cx = camera_matrix[0,2]
 cy = camera_matrix[1,2]
 
 # ---------------------------------------------------------------------------------
-# Step 2. Find the coordinate of point p (xim, yim) in image coordinate system.
-# done in object detection part; use dummy points for now.
+# Paso 2. Encontrar la coordenada del punto p (xim, yim) en el sistema de coordenadas de la imagen.
+# hecho en la parte de detección de objetos; usar puntos ficticios por ahora.
 
-# get set of dummy points
+# obtener un conjunto de puntos ficticios
 # image_points = np.array([[ 173.54767, 91.62902 ],
 # [ 210.7686, 92.32755 ],
 # [ 247.47551, 92.77607 ],
@@ -76,23 +76,23 @@ image_points = np.array([[ 171.63728, 127.19049 ],
 [ 432.02014, 132.50407 ],
 [ 469.7123, 133.17534 ]])
 
-# convert the image points to homogeneous coordinates
+# convertir los puntos de la imagen en coordenadas homogéneas
 image_points_hom = np.concatenate((image_points, np.ones((image_points.shape[0], 1))), axis=1)
 # print("Homogeneous image points: \n", image_points_hom)
 
 # ---------------------------------------------------------------------------------
-# Step 3. Find the coordinate of projection center Oc in real world coordinate 
-# system using Eq. (3).
+# Paso 3. Hallar la coordenada del centro de proyección Oc en el sistema de coordenadas 
+# del mundo real utilizando la Ec. (3).
 
-## get the rotation and translation vectors corresponding to the static camera field of view
+# obtener los vectores de rotación y traslación correspondientes al campo de visión estático de la cámara
 rotation_vector = np.array([2.10602313, 2.15303455, -0.19642816]).reshape(3,1)
 translation_vector = np.array([-122.22738712, -117.38511046, 656.31453705]).reshape(3,1)
 
-# finding the camera centre Cc in real world coordinate system.
-## get the rotation matrix from rotation vector
+# encontrar el centro de la cámara Cc en el sistema de coordenadas del mundo real.
+## obtener la matriz de rotación a partir del vector de rotación
 rotation_matrix, _ = cv.Rodrigues(rotation_vector)
 
-## camera centre C = -R^(-1).t
+## centro de la cámara C = -R^(-1).t
 C = - np.linalg.inv(rotation_matrix) @ translation_vector
 # print("Camera Centre in world coordinate: \n", C)
 
@@ -101,10 +101,10 @@ for point in image_points_hom:
     print("Image point: ", np.round(point, decimals=2))
 
     # ---------------------------------------------------------------------------------
-    # Step 4. Find the coordinate of p (ximw, yimw, zimw) in real world 
-    # coordinate system using Eq. (7)
+    # Paso 4. Encontrar la coordenada de p (ximw, yimw, zimw) en el mundo real 
+    # utilizando la Ec. (7)
 
-    # from eq. 4, 5, 6
+    # de las ec. 4, 5, 6
     x_imc = (point[0] - cx)/sx
     y_imc = (point[1] - cy)/sy
     z_imc = 1* focul_length_in_mm
@@ -112,23 +112,23 @@ for point in image_points_hom:
     point_in_camera_coordinate = np.array([x_imc, y_imc, z_imc]).reshape(3,1)
     # print("Point in camera coordinate: \n", point_in_camera_coordinate)
 
-    # from eq. 7
+    # de la ec. 7
     point_in_world_coordinate = np.linalg.inv(rotation_matrix) @ (point_in_camera_coordinate - translation_vector)
     # print("Point in world coordinate: \n", point_in_world_coordinate)
 
-    # equation of the plane where the point lies in real world coordinate system
-    # equa 10: Z = 0 plane. store it in an array in standard form ax+by+cz = d
+    # ecuación del plano donde se encuentra el punto en el sistema de coordenadas del mundo real
+    # ecuación 10: Plano Z = 0. Almacenarlo en una matriz en forma estándar ax+by+cz = d
     plane_equation = np.array([0, 0, 1, 0]) # a, b, c, d
     
-    # calculate the parameter t using eq. 11 
+    # Calculo del parametro t usando la ec. 11 
     t = (plane_equation[-1] - np.dot(plane_equation[0:3], C)) / np.dot(plane_equation[0:3], (point_in_world_coordinate - C))
     # print("t: ", t)
 
     # ---------------------------------------------------------------------------------
-    # Step 5. Approximate the coordinate of P in real world coordinate system using Eq.
-    # (12), (13), and (14).
+    # Paso 5. Aproximar la coordenada de P en el sistema de coordenadas del mundo real utilizando las Ec.
+    # (12), (13) y (14).
 
-    # from eq. 12, 13, 14
+    # de las ec. 12, 13, 14
     approximated_world_coord = C + t * (point_in_world_coordinate - C)
     print("Approx: world coord: ", np.round(approximated_world_coord.reshape(3,), decimals=2))
     print("\n", '-'*50)
